1803113aa29abc8dabde389d802d33ef6e6392fb
ruirui.qu <ruirui.qu@qunar.com>
2019-05-27 10:07:15 +0800

    add

---
+++ src/main/java/util/concurrent/App.java
@@ -0,0 +1,14 @@
+package util.concurrent;
+
+//--------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 2019/5/27<p>
+//-------------------------------------------------------
+//--------------------- Change Logs----------------------
+//  <p>@author ruirui.qu Initial Created at 2019/5/27</p>
+//-------------------------------------------------------
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("===》");
+    }
+}
